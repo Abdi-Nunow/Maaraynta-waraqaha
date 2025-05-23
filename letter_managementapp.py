f14ae1a956c1caad32a4a964c7d258771ea168dd
Abdi-Nunow <101399179+Abdi-Nunow@users.noreply.github.com>
2025-05-23 22:14:26 +0300

    letter_managementapp.py

--- letter_managementapp.py
+++ letter_managementapp.py
@@ -18,6 +18,7 @@ waaxyo = [
     "Wasiir Ku-xigeenka 2aad",
     "Wasiir Ku-xigeenka 3aad",
     "secratory",
+      "Waaxda Finance",
     "Waaxda Xadaynta",
     "Waaxda Auditka",
     "Waaxda Adeega Shacabka",
